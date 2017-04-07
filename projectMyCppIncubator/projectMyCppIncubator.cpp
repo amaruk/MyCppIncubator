4fab4333e9a25213b8c79a49d2472f31e9ceac08
amaruk <amaruk@163.com>
2017-04-07 09:43:53 +0800

    Update the CPP source file to test VS2017 configuration.

--- projectMyCppIncubator/projectMyCppIncubator.cpp
+++ projectMyCppIncubator/projectMyCppIncubator.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
 
 
-int main()
+int main(int argc, char *argv[])
 {
+	cout << "Hello, my C++ incubator" << endl;
     return 0;
 }
 
