e02727468015a79bff0879be56b79dd396349c53
amaruk <amaruk@163.com>
2017-05-05 15:23:15 +0800

    Update sequence container test.

--- projectMyCppIncubator/StdLibContainer.cpp
+++ projectMyCppIncubator/StdLibContainer.cpp
@@ -81,29 +81,72 @@ void StdLibContainer::sequenceContainer(void)
         ****构造函数****
         C c;                    默认构造函数，构造空容器
         C c1(c2);               构造c2的拷贝c1。array可拷贝，但内置数组类型不能拷贝。
-        C c1 = c2;              同上
+        C c1 = c2;              同上，赋值后c1的size变为c2的size
         C c(b, e);              构造c，将迭代器b（第一个拷贝的元素位置）和e（拷贝的最后一个元素之后的位置）指定范围的元素拷贝到c，类型相容即可，不需相同（array不支持）
         C c{a, b, c...};        C++11列表初始化c
-        C c = {a, b, c...};     C++11同上
+        C c = {a, b, c...};     C++11同上，赋值后c1的size变为c2的size
         C c(n);                 顺序容器（array除外）包含n个元素，进行值初始化。此构造函数是explicit的。
         C c(n, t);              同上，但初始化为t
-        ****赋值与swap****
-        c1 = c2;                z将c1中的元素替换为c2
-        c1 = {a, b, c...};      z将c1中的元素替换为列表中元素（array不支持）
-        a.swap(b);              z交换a和b的元素
-        swap(a, b);             z与a.swap(b)等价
         ****大小****
         c.size();               c中元素数目（不支持forward_list）
         c.max_size();           c可保存的最大元素数目
         c.empty();              若c中存储了元素，返回false；否则返回true
+    */
+    /*
+        ****赋值与swap****
+        c1 = c2;                将c1中的元素替换为c2
+        c1 = {a, b, c...};      将c1中的元素替换为列表中元素（原书第五版提示array不支持，VS2017支持）
+                                *对array进行swap，耗时和array元素数量成正比，因为实际交换元素
+                                *对其他容器进行swap，速度很快为线性时间，因为不是拷贝元素而是交换了两个容器的内部数据结构
+        a.swap(b);              交换a和b的元素，早期版本之提供此类型
+        swap(a, b);             与a.swap(b)等价，在范性编程中很重要，建议使用此种非成员版本的swap
+                                *assign用于类型不同但相容的赋值，或用子序列赋值。不适用于关联容器和array
+        seq.assign(b, e);       将seq的元素替换为迭代器b和e所表示范围内的元素，迭代器b和e不能指向seq的元素（因为旧元素会被替换）
+        seq.assign(il);         将seq的元素替换为初始化列表il中的元素
+        seq.assign(n, t);       将seq的元素替换为n个值为t的元素
+    */
+    // 注意赋值操作会导致容器内部的迭代器、引用、指针失效
+    vector<int> vectorInt10 = { 0,1,2,3,4,5,6,7,8,9 };
+    vector<int> vectorInt5 = { 4,3,2,1,0 };
+    cout << "vectorInt10 size [" << vectorInt10.size() << "] " <<
+        "vectorInt5 size [" << vectorInt5.size() << "]" << endl;
+    vectorInt10 = vectorInt5; // 不同大小的容器赋值，左边的容器大小变为右边容器大小
+    cout << "vectorInt10 size [" << vectorInt10.size() << "] " <<
+        "vectorInt5 size [" << vectorInt5.size() << "]" << endl;
+
+    array<int, 10> aryInt10 = { 0,1,2,3,4,5,6,7,8,9 };
+    array<int, 10> aryInt10Another = { 9,8,7,6,5,4,3,2,1,0 };
+    array<int, 5> aryInt5 = { 4,3,2,1,0 };
+    //aryInt10 = aryInt5; // array赋值必须类型相同
+    aryInt10 = aryInt10Another; // 类型相同，可以赋值
+    cout << "before " << aryInt5[0] << aryInt5[1] << aryInt5[2] << aryInt5[3] << aryInt5[4] << endl;
+    aryInt5 = { 1,2 };
+    cout << "after " << aryInt5[0] << aryInt5[1] << aryInt5[2] << aryInt5[3] << aryInt5[4] << endl;
+
+    /*
+        ****关系运算符****
+        *运算符两边的容器必须类型相同，元素类型相同
+        ==, !=                  所有容器都支持
+        <, <=, >, >=            关系运算符，无序关联容器不支持
+                                比较规则：
+                                - 两个容器大小相同且所有元素亮亮对应相等，则容器相等；否则不等
+                                - 两个容器大小不同，较小容器中每个元素都等于较大容器中对应元素，则较小容器小于较大容器
+                                - 两个容器都不是另一个容器的前缀子序列，则比较结果取决于第一个不相等的元素比较结果
+    */
+    vector<int> vector1 = { 1,3,5,7,9,12 };
+    vector<int> vector2 = { 1,3,9 };
+    vector<int> vector3 = { 1,3,5,7 };
+    vector<int> vector4 = { 1,3,5,7,9,12 };
+    cout << "vector1 < vector2: " << ((vector1 < vector2) ? "true" : "false") << endl; // 结果取决于元素[2]
+    cout << "vector1 < vector3: " << ((vector1 < vector3) ? "true" : "false") << endl; // 元素都相等，vector3元素数量少
+    cout << "vector1 == vector4: " << ((vector1 == vector4) ? "true" : "false") << endl; //大小相同，元素相等
+    cout << "vector1 == vector2: " << ((vector1 == vector2) ? "true" : "fasle") << endl; // vector2元素数量少
+    /*
         ****添加删除元素（不适用array）****
         c.insert(args);         将args中元素拷贝进c
         c.emplace(inits);       使用inits构造c中的一个元素
         c.erase(args);          删除args指定的元素
         c.clear();              删除c中所有元素，返回void
-        ****关系运算符****
-        ==, !=                  所有容器都支持等于、不等于运算符
-        <, <=, >, >=            关系运算符，无序关联容器不支持
         ****获取迭代器****
         c.begin(); c.end();     返回指向c的首元素和尾元素之后位置的迭代器iterator
         c.cbegin(); c.cend();   返回const_iterator
