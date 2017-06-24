8e6f6d414b573ee9dfdd47d16879dd6f607295f2
amaruk <amaruk@163.com>
2017-06-24 07:42:48 +0200

    VS requires to unify line end. Update first to see what's required by github.

--- projectFindStrInTxt/projectFindStrInTxt.cpp
+++ projectFindStrInTxt/projectFindStrInTxt.cpp
@@ -1,11 +1,26 @@
-// projectFindStrInTxt.cpp : Defines the entry point for the console application.
+﻿// projectFindStrInTxt.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <fstream>
 
+using std::ifstream;
+
+void runQuery(ifstream &inFile)
+{
+    // 创建查询类实例
+
+
+    // 提示用户输入查询的单词
+
+}
 
 int main()
 {
+    ifstream iFileToBeQueried; // 创建文件流但不绑定文件
+    iFileToBeQueried.open(".\\Resources\\queryMe.txt"); // 以默认模式绑定文件
+    if (iFileToBeQueried.is_open())
+    { runQuery(iFileToBeQueried); }
     return 0;
 }
 
