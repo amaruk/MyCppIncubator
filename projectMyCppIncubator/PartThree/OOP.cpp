9dcb2d78b0597560a6ab097d9ae40e5613b37782
amaruk <amaruk@163.com>
2017-07-03 10:26:03 +0200

    Add base class and derived class.

--- projectMyCppIncubator/PartThree/OOP.cpp
+++ projectMyCppIncubator/PartThree/OOP.cpp
@@ -1,30 +1,77 @@
 ﻿#include "stdafx.h"
 #include "OOP.h"
-#include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
 
-OperatingSystem::OperatingSystem()
+
+void OperatingSystem::osRootInfo(void)
 {
+    cout << "OS ROOT INFO" << endl;
 }
 
-
-OperatingSystem::~OperatingSystem()
+void OperatingSystem::dispOsName(void) const
 {
+    cout << "OS name: " << osName << endl;
+}
+
+void OperatingSystem::dispOsVer(void) const
+{
+    cout << "OS ver: " << osVer << endl;
+}
+
+void OperatingSystem::dispOsOpenSource(void) const
+{
+    cout << "OS open source: " << osIsOpen << endl;
+}
+
+const std::string OSWindows::companyName = string("Microsoft");
+void OSWindows::dispOsName(void) const
+{
+    cout << OSWindows::companyName << " Product: " << osName << ". Embedded: " << isEmbedded << endl;
+}
+
+void printOsInfo(OperatingSystem &osIns)
+{
+    osIns.dispOsName();
+    osIns.dispOsVer();
+    osIns.dispOsOpenSource();
 }
 
 void testOOP(void)
 {
     /*
-        面向对象程序设计三个基本概念：
-        - 数据抽象： 把现实生活的事物抽象为类，将类的接口与实现分离
-        - 继承：定义相似的类型并对其相似关系建模
-        - 动态绑定：在一定程度忽略类型区别，以统一的方式使用它们的对象
+    面向对象程序设计三个基本概念：
+    - 数据抽象： 把现实生活的事物抽象为类，将类的接口与实现分离
+    - 继承：定义相似的类型并对其相似关系建模
+    - 动态绑定：在一定程度忽略类型区别，以统一的方式使用它们的对象
 
-        继承inheritance关系的基础是基类Base class，其他类直接或间接从基类继承，称为派生类derived class
+    继承inheritance关系的基础是基类Base class，其他类直接或间接从基类继承，称为派生类derived class
 
-        基类把希望派生类各自实现的函数声明为虚函数virtual function
+    动态绑定dynamic binding：又名运行时绑定run-time binding。
+    使用基类的引用或指针调用虚函数时，动态决定调用哪个类的函数。即把派生类对象当作基类对象使用。
+    称为派生类到基类derived-to-base类型转换
+
+    静态类型static type：编译时已知，是变量声明是的类型或表达式生成的类型
+    动态类型dynamic type：变量或表达式表示的内存中对象的类型
+    表达式如果不是引用也不是指针，其动态类型和静态类型永远一致
+
+    直接基类direct base，派生类对于基类
+    间接基类indirect base，派生类的派生类对于基类
+
+    用基类的默认（合成）拷贝和赋值操作派生类对象，只能操作基类定义的成员，派生类独有的成员被切除sliced down
+
     */
-    cout << "Test OperatingSystem" << endl;
+    OperatingSystem baseOS = OperatingSystem(string("Abstract OS"), 0, true);
+    OSWindows dervWin = OSWindows(string("Windows"), 10, false, false);
+
+    cout << "### Test virtual and override." << endl;
+    cout << endl << "<OS CLASS:>" << endl;
+    printOsInfo(baseOS);
+    cout << endl << "<WINDOWS CLASS:>" << endl;
+    printOsInfo(dervWin);
+    cout << endl;
+
 }
--- projectMyCppIncubator/PartThree/OOP.h
+++ projectMyCppIncubator/PartThree/OOP.h
@@ -1,9 +1,55 @@
 ﻿#pragma once
+#include <iostream>
+
+// 基类
 class OperatingSystem
 {
-public:
-    OperatingSystem();
-    ~OperatingSystem();
+    // 基类把希望派生类各自实现的函数声明为虚函数virtual function
+    // 构造函数之外的非静态函数可以为虚函数
+    // virtual只能出现在类内部
+    // 基类中的虚函数，在派生类中隐式地是虚函数
+public: // 派生类可以访问，其他用户可以访问
+    OperatingSystem() = default;
+    OperatingSystem(std::string name, int ver, bool open) : osName(name), osVer(ver), osIsOpen(open) {};
+    virtual ~OperatingSystem() = default; // 继承关系中的根节点类一般定义虚析构函数
+
+    // 虚函数
+    virtual void dispOsName(void) const;
+    virtual void dispOsVer(void) const;
+    virtual void dispOsOpenSource(void) const;
+
+    static void osRootInfo(void);
+private: // 派生类不可访问，其他用户不可访问
+protected: // 派生类可以访问，其他用户不可访问
+    std::string osName;
+    int osVer;
+    bool osIsOpen;
 };
+
+// 派生类
+// 通过类派生列表class derivation list指明从哪些基类继承而来。
+// 每个基类前面可以有访问说明符：public/protected/private
+// 派生类在其他文件中的类声明不需要包含派生列表
+class OSWindows final : public OperatingSystem // c++11：final的类不能被继承
+{
+public:
+    OSWindows() = default;
+    // 派生类不能直接初始化继承来的成员，要调用基类的构造函数
+    // 不显式初始化的话，基类的成员会默认初始化
+    OSWindows(std::string name, int ver, bool open, bool embedded) : OperatingSystem(name, ver, open), isEmbedded(embedded) {};
+    ~OSWindows() = default;
+
+    // 派生类必须对所有重新定义的虚函数进行声明，virtual关键字可选
+    // c++11: override关键字表示派生类显式注明用此成员函数改写基类的虚函数
+    virtual void dispOsName(void) const override; // 覆盖虚函数
+    //virtual void dispOsVer(void) const override; // 不覆盖的虚函数继承基类
+    //virtual void dispOsOpenSource(void) const override;
+
+private:
+    static const std::string companyName; // 静态成员
+    bool isEmbedded;
+};
+
+
 
 void testOOP(void);
