739b08d739f0790a42d8dbf34b63a8d36e86389a
amaruk <amaruk@163.com>
2017-11-12 09:43:34 +0100

    Add more description about lvalue and rvalue.

--- EffectiveModernCpp/Item25.cpp
+++ EffectiveModernCpp/Item25.cpp
@@ -18,5 +18,7 @@ void item25()
   // 如果要传universial reference，因为不一定为左值引用还是右值引用，用forward来根据初始化的情况来转右值或左值再使用
 
   // 由于RVO（Return Value Optimization）的存在，不要move或forward会用作RVO的局部变量
+  // RVO是在要返回局部变量的情况下，为避免拷贝，编译器把要返回的变量直接生成到用于返回的内存位置
+  // NRVO (Named Return Value Optimization) 返回未命名的局部变量
 
 }
--- projectMyCppIncubator/PartThree/ClassFeatures.cpp
+++ projectMyCppIncubator/PartThree/ClassFeatures.cpp
@@ -103,12 +103,25 @@ void testClassFeatures(void)
     */
 
     /*
-        对象作为左值时，用其身份（内存中的位置，可以出现在表达式等号左边，如命名的变量）
-        对象作为右值时，用其值（内容，只能出现在等号右边，如临时变量）
-        例：
-            赋值运算符左侧为非常量的左值，其结果也是左值
-            取地址运算符作用于左值，返回的指针是右值
-            解引用，下标的求值结果是左值
+        C++ 98/03规范定义：every expression is either an lvalue or an rvalue.
+        所以左值和右值是表达式的属性而非对象的属性，不在乎表达式做了什么，而是命名了什么（参考++x和x++的区别）
+
+        左值：lvalues name object that persist beyond a single expression.
+              表达式命名生命周期超过单条表达式的对象，如obj, *ptr, ptr[index], ++x
+        右值：rvalues are temporaries that evaporate at the end of the full-expression in which they live.
+              表达式执行完后结束生命周期的临时变量。如1729, x+y, std::string("neow"), x++
+        x++表达式先拷贝x，自增x，然后返回拷贝的临时变量，所以是右值。
+        ++x则是自增之后返回x，所以是左值
+
+        分辨的方法：能否获取表达式得到的对象的地址，如可以则为左值。
+        因为规范定义取地址操作符"operand shall be an lvalue"。
+
+        对于函数，规范定义"A function call is an lvalue if and only if the result is a reference.
+        例：vector<int> v(10,1729);
+            v[0]是左值，因为操作符[]()返回int&
+        例：string s("foo");
+            string t("bar");
+            s+t是右值，因为操作符+()返回string
 
         左值引用 &：不能绑定到要求转换的表达式/字面常量/返回右值的表达式
         右值引用 &&：与左值引用完全相反，只能绑定到右值
@@ -125,7 +138,7 @@ void testClassFeatures(void)
 
         例：
         T& t1 = v;  // t1是类型为引用的变量，指向左值，称为左值引用，t1自身为左值
-        T&& t2 = func(); // t2是右值引用，t2自身为左值
+        T&& t2 = func(); // t2是右值引用，t2自身为左值，因为引用是一个名字，即使绑定到右值，其自身已然是左值
         注意如果t2定义语句里的T为模板参数、auto、decltype等需要推导的类型，
         则t2是universal reference，或forwarding reference，根据reference collapsing
         及右值引用推导原则，t2被左值初始化时为左值引用，被右值初始化时为右值引用。
