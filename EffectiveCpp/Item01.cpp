20e1ad30ba1f2df143c1a2b1ba929459eac43a94
amaruk <amaruk@163.com>
2017-11-19 10:11:19 +0100

    Add Item 01.

---
+++ EffectiveCpp/Item01.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "common.h"
+#include "Item01.h"
+
+
+Item01::Item01()
+{
+}
+
+
+Item01::~Item01()
+{
+}
+
+void ExampleItem01()
+{
+  // View C++ as a federation of languages
+  // 可以把C++分为4部分：
+  // - C：基于C的部分，兼容C的考虑
+  // - Object-Oriented C++：关于类的特性
+  // - Template C++：模板和泛型
+  // - The STL：标准模板库
+  cout << "===== Example Item 01 =====" << endl;
+}
---
+++ EffectiveCpp/Item01.h
@@ -0,0 +1,9 @@
+#pragma once
+class Item01
+{
+public:
+  Item01();
+  ~Item01();
+};
+
+void ExampleItem01();
