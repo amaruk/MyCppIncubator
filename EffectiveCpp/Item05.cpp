c9986a3d0c765b75c8e6b9985ea3d065bfa0ac65
amaruk <amaruk@163.com>
2017-11-21 08:51:36 +0100

    Add item 05.

---
+++ EffectiveCpp/Item05.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "Item05.h"
+
+
+void ExampleItem05()
+{
+  ///// Item 5: Know what functions C++ silently writes and calls 
+  // 编译器默认生成的构造函数等是public和inline的。
+  cout << "===== Example Item 05 =====" << endl;
+
+}
