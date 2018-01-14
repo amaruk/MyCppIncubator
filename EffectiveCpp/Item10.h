3e8204c7559b266d40c1457d0be296da2d9d0a1b
amaruk <amaruk@163.com>
2018-01-14 12:35:36 +0100

    Add item 10 and 11.

---
+++ EffectiveCpp/Item10.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ItemBase.h"
+
+class Item10 :
+  public ItemBase
+{
+public:
+  Item10();
+  ~Item10();
+
+  void ItemEntry() override;
+};
+
---
+++ EffectiveCpp/Item11.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "ItemBase.h"
+class Item11 :
+  public ItemBase
+{
+public:
+  Item11();
+  ~Item11();
+  void ItemEntry() override;
+};
+
