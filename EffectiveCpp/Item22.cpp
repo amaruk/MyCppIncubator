6a19a290eca43654fbde498c3029966b80364f66
amaruk <amaruk@163.com>
2018-01-19 07:50:44 +0100

    Add Item 21/22/23.

---
+++ EffectiveCpp/Item22.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Item22.h"
+
+
+Item22::Item22() :
+  ItemBase("22")
+{
+}
+
+
+Item22::~Item22()
+{
+}
+
+void Item22::ItemEntry()
+{
+  //// Item 22: Declare data members private
+  // private体现的封装的好处，成员私有之后，类内部实现可以随意变化，
+  // 只要接口定义和接口行为不变，用户就不用改变他们的代码
+}
---
+++ EffectiveCpp/Item23.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ItemBase.h"
+class Item23 :
+  public ItemBase
+{
+public:
+  Item23();
+  ~Item23();
+
+  void ItemEntry() override;
+};
+
