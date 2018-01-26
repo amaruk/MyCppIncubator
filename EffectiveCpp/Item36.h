3e8371eee9a49c1e2e78072b37dca433e8655a40
amaruk <amaruk@163.com>
2018-01-26 08:02:53 +0100

    Add item 36/37.

---
+++ EffectiveCpp/Item36.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ItemBase.h"
+class Item36 :
+  public ItemBase
+{
+public:
+  Item36();
+  ~Item36();
+
+  void ItemEntry() override;
+};
+
---
+++ EffectiveCpp/Item37.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ItemBase.h"
+class Item37 :
+  public ItemBase
+{
+public:
+  Item37();
+  ~Item37();
+
+  void ItemEntry() override;
+};
+
