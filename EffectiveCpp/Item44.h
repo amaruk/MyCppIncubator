e3e28b04d60c992ce191009e07c47e23d44da25a
amaruk <amaruk@163.com>
2018-01-30 07:52:28 +0100

    Add item 44/45.

---
+++ EffectiveCpp/Item44.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ItemBase.h"
+class Item44 :
+  public ItemBase
+{
+public:
+  Item44();
+  ~Item44();
+
+  void ItemEntry() override;
+};
+
---
+++ EffectiveCpp/Item45.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ItemBase.h"
+class Item45 :
+  public ItemBase
+{
+public:
+  Item45();
+  ~Item45();
+
+  void ItemEntry() override;
+};
+
