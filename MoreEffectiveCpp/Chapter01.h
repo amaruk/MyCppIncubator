bf6a3530b0c9a0c889757a45c0536fcd99f52f49
amaruk <amaruk@163.com>
2018-03-05 08:05:13 +0100

    Add item 02.

--- MoreEffectiveCpp/Chapter01.h
+++ MoreEffectiveCpp/Chapter01.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "Item01.h"
+#include "Item02.h"
---
+++ MoreEffectiveCpp/Item02.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "Item02.h"
+
+Item02::Item02() :
+  ItemBase("02")
+{
+}
+
+
+Item02::~Item02()
+{
+}
+
+
+void Item02::ItemEntry()
+{
+  // Item02: Prefer C++-style casts.
+
+  // static_cast类似于C的类型转换。不能转换constness。
+  // const_cast用于只转换constness和volatileness。
+  // dynamic_cast只用于在继承层级中，把指针或引用从基类转换为派生类或兄弟类，并可通过检测转换结果是否为null来判断转换是否成功。
+  // reinterpret_cast和底层实现绑定，移植性差
+
+  // 对于shared_ptr用对应的：
+  // static_pointer_cast, dynamic_pointer_cast, const_pointer_cast, reinterpret_pointer_cast
+}
