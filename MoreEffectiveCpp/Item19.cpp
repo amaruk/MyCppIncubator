113585c74e41f755119b8110be81e09162c4e05c
amaruk <amaruk@163.com>
2019-05-13 21:55:36 +0800

    Add item 19 and 20.

---
+++ MoreEffectiveCpp/Item19.cpp
@@ -0,0 +1,12 @@
+﻿#include "stdafx.h"
+#include "Item19.h"
+
+void Item19::ItemEntry()
+{
+  // Item 19: Understand the origin of temporary objects.
+  // Temporary object不是指代码局部使用的临时变量，而是指不出现在代码里的真正的临时对象。
+  // 未命名的非heap对象即为临时对象。
+  // 两种情况下出现：
+  // 1. 在函数调用时，实参类型和行参类型不同导致的隐式类型转换时构造，函数调用语句结束时析构。
+  // 2. 函数返回对象
+}
---
+++ MoreEffectiveCpp/Item20.cpp
@@ -0,0 +1,8 @@
+﻿#include "stdafx.h"
+#include "Item20.h"
+
+void Item20::ItemEntry()
+{
+  // Item 20: Facilitate “the return value optimization”.
+  // 返回对象的函数会导致临时对象的创建，但如果return语句后跟构造函数的调用，则不会创建临时对象。
+}
